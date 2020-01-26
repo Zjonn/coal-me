277dd69ae5fd19559e63b693b0426e6882cb8c4d
zjonn <zjonnn@gmail.com>
2020-01-26 22:23:30 +0100

    Diesel added

--- src/main.rs
+++ src/main.rs
@@ -4,30 +4,38 @@
 //!
 //! [actix]: https://actix.rs/docs/
 
-use actix_web::http::{header, Method, StatusCode};
-use actix_web::middleware::session::{self, RequestSession};
-use actix_web::{
-    error, fs, middleware, pred, server, App, Error, HttpRequest, HttpResponse, Path, Responder,
-    Result,
-};
+#[macro_use]
+extern crate lazy_static;
+
+use chrono;
+
+use actix_files as fs;
+use actix_identity::{CookieIdentityPolicy, IdentityService};
+
+use actix_web::http::{StatusCode};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder, Result};
+
 use std::env;
-use std::path;
 
-fn welcome(req: &HttpRequest) -> Result<HttpResponse> {
-    Ok(HttpResponse::build(StatusCode::OK)
-        .content_type("text/html; charset=utf-8")
-        .body(include_str!("../static/index.html")))
-}
+// fn welcome(req: &HttpRequest) -> Result<HttpResponse> {
+//     Ok(HttpResponse::build(StatusCode::OK)
+//         .content_type("text/html; charset=utf-8")
+//         .body(include_str!("../static/index.html")))
+// }
 
-fn static_data(req: &HttpRequest) -> Result<HttpResponse> {
-    Ok(HttpResponse::build(StatusCode::OK)
-        .content_type("text/html; charset=utf-8")
-        .body(include_str!("../static/index.html")))
-}
+// fn static_data(req: &HttpRequest) -> Result<HttpResponse> {
+//     Ok(HttpResponse::build(StatusCode::OK)
+//         .content_type("text/html; charset=utf-8")
+//         .body(include_str!("../static/index.html")))
+// }
 
-fn greet(req: &HttpRequest) -> impl Responder {
-    let to = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", to)
+// fn greet(req: &HttpRequest) -> impl Responder {
+//     let to = req.match_info().get("name").unwrap_or("World");
+//     format!("Hello {}!", to)
+// }
+
+lazy_static::lazy_static! {
+pub  static ref SECRET_KEY: String = "CoalME".repeat(8);
 }
 
 fn main() {
@@ -38,16 +46,26 @@ fn main() {
         .expect("PORT must be a number");
 
     // Start a server, configuring the resources to serve.
-    server::new(|| {
+    HttpServer::new(|| {
         App::new()
-            .handler("/", fs::StaticFiles::new("./static/").unwrap().index_file("index.html"))
-            .resource("/test", |r| r.f(welcome))
-            .resource("/static/{res}", |r| r.f(static_data))
-            .resource("/req/{name}", |r| r.f(greet))
+            .wrap(IdentityService::new(
+                CookieIdentityPolicy::new(SECRET_KEY.as_bytes())
+                    .name("auth")
+                    .path("/")
+                    .secure(true)
+                    .domain("https://coal-me.herokuapp.com/")
+                    .max_age_time(chrono::Duration::hours(12)),
+            ))
+            .service(fs::Files::new("/logged", "./static/logged").index_file("index.html"))
+            .service(fs::Files::new("/", "./static/").index_file("index.html"))
             
-            
+            //.service()
+            .default_service(web::route().to(|| HttpResponse::NotFound()))
+        // .resource("/test", |r| r.f(welcome))
+        // .resource("/static/{res}", |r| r.f(static_data))
+        // .resource("/req/{name}", |r| r.f(greet))
     })
-    .bind(("0.0.0.0", port))
+    .bind(("localhost", port))
     .expect("Can not bind to port 8000")
     .run();
 }
